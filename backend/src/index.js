dfa174789970890a1b8dc11f839ae3498e670b7c
otaciliob <otaciliobarbieri@gmail.com>
2023-06-01 21:00:43 -0300

    construção das paginas

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,4 +2,13 @@ const app = require("./app");
 
 app.get('/',(req,res)=>res.status(200).send("Hello World!"));
 
+app.get("/setcookie", (req, res) => {
+    res.cookie("Bearer", "A1B2C3D4");
+    res.send("Cookie criado com sucesso!");
+  });
+  app.get("/listcookie", (req, res) => {
+    console.log(req.cookies);
+    res.send(req.cookies.nome_cookie);
+  });
+
 app.listen(3000,()=> console.log("listen..."))
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,14 +1,17 @@
 const express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('index');
-  });
-  router.get('/page2', function(req, res, next) {
-    res.render('page2');
-  });
-  router.get('/page3', function(req, res, next) {
-    res.render('page3');
-  });
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+router.get('/page2', function (req, res, next) {
+  res.render('page2');
+});
+router.get('/page3', function (req, res, next) {
+  res.render('page3');
+});
+router.post('/', function (req, res, next) { });
 
-  module.exports = router;
+
+
+module.exports = router;
