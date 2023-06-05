4f2a9d4f4dd8a0eda86789a193e95174d79debe5
otaciliob <otaciliobarbieri@gmail.com>
2023-06-05 20:51:35 -0300

    new pages

--- backend/src/router.js
+++ backend/src/router.js
@@ -10,6 +10,12 @@ router.get('/page2', function (req, res, next) {
 router.get('/page3', function (req, res, next) {
   res.render('page3');
 });
+router.get('/page4', function (req, res, next) {
+  res.render('page4');
+});
+router.get('/page5', function (req, res, next) {
+  res.render('page5');
+});
 router.post('/', function (req, res, next) { });
 
 
