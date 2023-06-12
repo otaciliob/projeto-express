3a549d2ab3ffdf79255f34cc37a3c32567b0ec68
otaciliob <48457718+otaciliob@users.noreply.github.com>
2023-06-12 20:40:43 -0300

    Update app.js
    morgan

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mustacheExpress = require("mustache-express");
 const path = require('path');
+var logger = require('morgan');
 
 const app = express();
 const engine = mustacheExpress();
 var indexRouter = require('./router');
 
+app.use(logger('dev'));
 app.use(express.urlencoded())
 app.use(express.json())
 app.engine("mustache", engine);
-app.use(express.static("./src/images"))
 app.set('views', path.join(__dirname, 'views'));
 //app.set('images', path.join(__dirname, 'images'));
 app.set('view engine', 'mustache');
+app.use(express.static("./src/images"))
 
 app.use('/', indexRouter);
 
