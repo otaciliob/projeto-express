2a565262d06dbb16cd0f937b5e2436456f2005ea
otaciliob <48457718+otaciliob@users.noreply.github.com>
2023-06-13 20:50:08 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const mustacheExpress = require("mustache-express");
 const path = require('path');
 var logger = require('morgan');
@@ -6,16 +7,21 @@ var logger = require('morgan');
 const app = express();
 const engine = mustacheExpress();
 var indexRouter = require('./router');
+var cookieRouter = require('./index');
 
 app.use(logger('dev'));
 app.use(express.urlencoded())
 app.use(express.json())
+app.use(cookieParser())
 app.engine("mustache", engine);
 app.set('views', path.join(__dirname, 'views'));
-//app.set('images', path.join(__dirname, 'images'));
+app.set('images', path.join(__dirname, 'javascript'));
 app.set('view engine', 'mustache');
 app.use(express.static("./src/images"))
 
 app.use('/', indexRouter);
+app.use('/cookie/', cookieRouter);
+
+app.listen(3000,()=> console.log("listen..."))
 
 module.exports = app;
